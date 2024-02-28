e3a863a1166144fdebc81effcaaacf9b1964bba3
hyu939 <61054686+hyu939@users.noreply.github.com>
2024-02-28 19:28:00 +0700

    portfolio_v1

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let themeToggler = document.querySelector('#theme-toggler');
+    let body = document.body;
+
+    // Kiểm tra xem chế độ tối đã được kích hoạt hay chưa
+    let isDarkModeActivated = body.classList.contains('active');
+
+    // Cập nhật biểu tượng của nút chuyển đổi chế độ phản ánh trạng thái chế độ tối
+    if (isDarkModeActivated) {
+        themeToggler.classList.add('fa-sun');
+    } else {
+        themeToggler.classList.remove('fa-sun');
+    }
+
+    // Xử lý sự kiện khi nhấp vào nút chuyển đổi chế độ
+    themeToggler.onclick = () => {
+        themeToggler.classList.toggle('fa-moon');
+        body.classList.toggle('active');
+    }
+});
