fd9a5582c377f5c7f7e79a2c19f9334e1cfbef44
hyu939 <61054686+hyu939@users.noreply.github.com>
2024-02-29 01:12:18 +0700

    portfolio_v2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-let menu = document.querySelector('#menu-btn');
-let header = document.querySelector('.header');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-}
-
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    header.classList.remove('active');
-}
-
 document.addEventListener('DOMContentLoaded', function() {
+    let menu = document.querySelector('#menu-btn');
+    let header = document.querySelector('.header');
+
+    menu.onclick = () =>{
+        menu.classList.toggle('fa-times');
+        header.classList.toggle('active');
+    }
+
+    window.onscroll = () =>{
+        menu.classList.remove('fa-times');
+        header.classList.remove('active');
+    }
+
     let themeToggler = document.querySelector('#theme-toggler');
     let body = document.body;
 
